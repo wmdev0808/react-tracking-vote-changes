99800ba711c122a96781b899a0e3e65b7397e3b4
Paul Li <wmdev0808@gmail.com>
2022-09-07 04:20:36 -0400

    -- Scaffolds the project

--- README.md
+++ README.md
@@ -1,70 +1,36 @@
-# Getting Started with Create React App
+# Requirements
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+There are `n` items the user can vote on.
+There are two React states:
 
-## Available Scripts
+- `votes` is an array of the user's votes on every item.
 
-In the project directory, you can run:
+  ```
+  const [votes, setVotes] = useState({});
+  ```
 
-### `npm start`
+- `voteChanges` indicates how many times the user changed their votes on any of the items.
+  ```
+  const [voteChanges, setVoteChanges] = useState(0);
+  ```
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+We have a database listener defined in a `useEffect`. When votes are added/changed in the database, the listener gets called. We need the state `votes` to compare every new vote with its previous value in `votes` to decide whether to increment `voteChanges`. However, we cannot define `votes` as a dependency in `useEffect`, otherwise we will end up in an infinite loop. To get access to the current value of `votes`, we use its functional update. Is it okay to call `setVoteChanges` inside the functional update of `setVotes`? If no, why, and how would you solve this problem?
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+```
+useEffect(() => {
+  databaseListener((changes) => {
+    setVotes((oldVotes) => {
+      const oVotes = { ...oldVotes };
+      for (let change of changes) {
+        const itemId = change.id;
+        const newVote = change.vote;
+        if (oVotes[itemId] !== newVote) {
+          setVoteChanges(oldCount => oldCount + 1);
+        }
+        oVotes[itemId] = newVote;
+      }
+      return oVotes;
+    });
+  });
+});
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import { firebase } from "./firebase/firebase";
+
+const authenticatedUser = "Iman";
+
+export default function App() {
+  const [votes, setVotes] = useState({});
+  const [voteChanges, setVoteChanges] = useState(0);
+
+  // Snapshot listeners to retrieve and combine the messages and votes
+  useEffect(() => {
+    const messagesQuery = firebase.db.collection("messages");
+    const messagesSnapshot = messagesQuery.onSnapshot((snapshot) => {
+      const docChanges = snapshot.docChanges();
+      setVotes((oldVotes) => {
+        const oVotes = { ...oldVotes };
+        for (let change of docChanges) {
+          if (change.doc.id in oVotes) {
+            oVotes[change.doc.id] = {
+              ...oVotes[change.doc.id],
+              ...change.doc.data(),
+            };
+          } else {
+            oVotes[change.doc.id] = change.doc.data();
+          }
+        }
+        return oVotes;
+      });
+    });
+
+    const votesQuery = firebase.db
+      .collection("mVotes")
+      .where("voter", "==", authenticatedUser);
+    const votesSnapshot = votesQuery.onSnapshot((snapshot) => {
+      const docChanges = snapshot.docChanges();
+      setVotes((oldVotes) => {
+        const oVotes = { ...oldVotes };
+        for (let change of docChanges) {
+          const voteData = change.doc.data();
+          const itemId = voteData.messageId;
+          const newVote = voteData.userVote;
+          if (oVotes[itemId].userVote !== newVote) {
+            setVoteChanges((oldCount) => oldCount + 1);
+          }
+          if (itemId in oVotes) {
+            oVotes[itemId] = {
+              ...oVotes[itemId],
+              voter: voteData.voter,
+              userVote: newVote,
+            };
+          } else {
+            oVotes[itemId] = {
+              voter: voteData.voter,
+              userVote: newVote,
+            };
+          }
+        }
+        return oVotes;
+      });
+    });
+    return () => {
+      messagesSnapshot();
+      votesSnapshot();
+    };
+  }, []);
+
+  const upVote = (mId) => async (event) => {
+    const messageRef = firebase.db.collection("messages").doc(mId);
+    await firebase.db.runTransaction(async (t) => {
+      const messageDoc = await t.get(messageRef);
+      if (messageDoc.exists) {
+        const messageData = messageDoc.data();
+        const mVoteDocs = await firebase.db
+          .collection("mVotes")
+          .where("messageId", "==", mId)
+          .where("voter", "==", authenticatedUser)
+          .get();
+        if (mVoteDocs.docs.length > 0) {
+          const voteData = mVoteDocs.docs[0].data();
+          const mVoteRef = firebase.db
+            .collection("mVotes")
+            .doc(mVoteDocs.docs[0].id);
+          t.update(mVoteRef, {
+            userVote: !voteData.userVote,
+            dateTime: firebase.firestore.Timestamp.fromDate(new Date()),
+          });
+          t.update(messageRef, {
+            totalVotes: messageData.totalVotes + (voteData.userVote ? -1 : 1),
+          });
+        } else {
+          const mVoteRef = firebase.db.collection("mVotes").doc();
+          t.set(mVoteRef, {
+            messageId: mId,
+            voter: authenticatedUser,
+            userVote: true,
+            dateTime: firebase.firestore.Timestamp.fromDate(new Date()),
+          });
+          t.update(messageRef, {
+            totalVotes: messageData.totalVotes + 1,
+          });
+        }
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <strong>Total User Vote Changes: </strong>
+        {voteChanges}
+      </p>
+      <h1>Messages</h1>
+      <ul>
+        {Object.keys(votes).map((mId) => {
+          const message = votes[mId];
+          if (message.voter && message.message) {
+            return (
+              <li key={mId}>
+                <p>
+                  From <strong>{message.sender}</strong>, at{" "}
+                  {message.dateTime.toDate().toLocaleString()}
+                </p>
+                <p>{message.message}</p>
+                <div>
+                  <button
+                    style={{
+                      backgroundColor: message.userVote ? "green" : "#eeeeee",
+                    }}
+                    onClick={upVote(mId)}
+                  >
+                    <span role="img" aria-label="UpVote">
+                      �
+                    </span>{" "}
+                    {message.totalVotes}
+                  </button>
+                </div>
+              </li>
+            );
+          }
+        })}
+      </ul>
     </div>
   );
 }
-
-export default App;
